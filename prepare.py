3e27cdb6234e74d3affb7075b6a9349319f8dc6c
George Arredondo <george.arredondo87@gmail.com>
2020-10-06 15:33:32 -0500

    adding edits to prepare

--- prepare.py
+++ prepare.py
@@ -1,17 +1,17 @@
 import pandas as pd
-import acquire
+import acquire_g
 
 # Creating an acquire prep data function to clean and prep the paramaters that we will be using
 def acquire_and_prep_data():
     # Using the get_zillow_data function to acquire
-    df = acquire .get_zillow_data()
-
-    # Dropping missing data
-    df = df.dropna()
+    df = acquire_g.get_zillow_data()
 
     # Selecting the parameters for exploring
     df = df[['calculatedfinishedsquarefeet','bathroomcnt', 'bedroomcnt', 'regionidzip', 'fips', 'taxamount', 'taxvaluedollarcnt', 'yearbuilt']]
 
+    # Dropping missing data
+    df = df.dropna()
+    
     # Changing some data types as integers instead of floats
     df['bedroomcnt'] = df['bedroomcnt'].astype(int)
     df['calculatedfinishedsquarefeet'] = df['calculatedfinishedsquarefeet'].astype(int)
@@ -20,6 +20,6 @@ def acquire_and_prep_data():
     df['yearbuilt']=df['yearbuilt'].astype(int)
 
     # Renaming columns to read easier
-    df = df.rename(columns={'calculatedfinishedsquarefeet': 'sqft', 'regionidzip': 'zipcode'})
+    df = df.rename(columns={'calculatedfinishedcalcsquarefeet': 'sqft', 'regionidzip': 'zipcode'})
 
     return df
